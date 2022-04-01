1e3b0dd9a68071554d8361041572ff9dec4edb7a
lloydzhou <lloydzhou@qq.com>
2022-04-01 13:14:27 +0800

    移除watchEffect，移除lodash依赖

--- src/behaviors/Hoverable.ts
+++ src/behaviors/Hoverable.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
-import { watchEffect } from 'vue'
+import { onMounted, onUnmounted, defineComponent } from 'vue'
 import { useContext, contextSymbol } from '../GraphinContext'
-const Hoverable = {
+const Hoverable = defineComponent({
   name: 'Hoverable',
   props: {
     bindType: {
@@ -12,19 +12,21 @@ const Hoverable = {
   setup (props) {
     const { bindType = 'node' } = props
     const { graph } = useContext()
-    watchEffect((onInvalidate) => {
-      const handleNodeMouseEnter = (evt) => {
-        graph.setItemState(evt.item, 'hover', true)
-      }
-      const handleNodeMouseLeave = (evt) => {
-        graph.setItemState(evt.item, 'hover', false)
-      }
-      const handleEdgeMouseEnter = (evt) => {
-        graph.setItemState(evt.item, 'hover', true)
-      }
-      const handleEdgeMouseLeave = (evt) => {
-        graph.setItemState(evt.item, 'hover', false)
-      }
+
+    const handleNodeMouseEnter = (evt) => {
+      graph.setItemState(evt.item, 'hover', true)
+    }
+    const handleNodeMouseLeave = (evt) => {
+      graph.setItemState(evt.item, 'hover', false)
+    }
+    const handleEdgeMouseEnter = (evt) => {
+      graph.setItemState(evt.item, 'hover', true)
+    }
+    const handleEdgeMouseLeave = (evt) => {
+      graph.setItemState(evt.item, 'hover', false)
+    }
+
+    onMounted(() => {
       if (bindType === 'node') {
         graph.on('node:mouseenter', handleNodeMouseEnter)
         graph.on('node:mouseleave', handleNodeMouseLeave)
@@ -33,19 +35,20 @@ const Hoverable = {
         graph.on('edge:mouseenter', handleEdgeMouseEnter)
         graph.on('edge:mouseleave', handleEdgeMouseLeave)
       }
-      onInvalidate(() => {
-        if (bindType === 'node') {
-          graph.off('node:mouseenter', handleNodeMouseEnter)
-          graph.off('node:mouseleave', handleNodeMouseLeave)
-        }
-        if (bindType === 'edge') {
-          graph.off('edge:mouseenter', handleEdgeMouseEnter)
-          graph.off('edge:mouseleave', handleEdgeMouseLeave)
-        }
-      })
     })
+    onUnmounted(() => {
+      if (bindType === 'node') {
+        graph.off('node:mouseenter', handleNodeMouseEnter)
+        graph.off('node:mouseleave', handleNodeMouseLeave)
+      }
+      if (bindType === 'edge') {
+        graph.off('edge:mouseenter', handleEdgeMouseEnter)
+        graph.off('edge:mouseleave', handleEdgeMouseLeave)
+      }
+    })
+
     return () => null
   }
-}
+})
 
 export default Hoverable
